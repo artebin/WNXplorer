5fc259c54539403eadc53e715de30bbc6bf04b0b
nicolasj <nicolasj@94909ec9-e09f-23db-a905-0964dd708685>
2010-10-27 14:40:03 +0000

    rename the packages and the main is no longer in a separate package but in net.trevize.wnxplorer

---
+++ src/net/trevize/wnxplorer/explorer/GlobalKeyListener.java
@@ -0,0 +1,70 @@
+package net.trevize.wnxplorer.explorer;
+
+import java.awt.AWTEvent;
+import java.awt.event.AWTEventListener;
+import java.awt.event.KeyEvent;
+
+/**
+ * 
+ * 
+ * @author Nicolas James <nicolas.james@gmail.com> [[http://njames.trevize.net]]
+ * GlobalKeyListener.java - Feb 3, 2010
+ */
+
+public class GlobalKeyListener implements AWTEventListener {
+
+	private Explorer explorer;
+
+	public GlobalKeyListener(Explorer explorer) {
+		this.explorer = explorer;
+	}
+
+	public void eventDispatched(AWTEvent e) {
+		switch (e.getID()) {
+		case KeyEvent.KEY_PRESSED:
+			keyPressed((KeyEvent) e);
+			break;
+		case KeyEvent.KEY_RELEASED:
+			keyReleased((KeyEvent) e);
+			break;
+		case KeyEvent.KEY_TYPED:
+			keyTyped((KeyEvent) e);
+			break;
+		}
+	}
+
+	public void keyPressed(KeyEvent e) {
+		if (e.isAltDown() && e.getKeyChar() == '&') {
+			System.out.println("tab0");
+			explorer.getJtp().setSelectedIndex(0);
+		}
+
+		else
+
+		if (e.isAltDown() && e.getKeyChar() == 'é') {
+			System.out.println("tab1");
+			explorer.getJtp().setSelectedIndex(1);
+		}
+
+		else
+
+		if (e.isAltDown() && e.getKeyChar() == '"') {
+			System.out.println("tab2");
+			explorer.getJtp().setSelectedIndex(2);
+		}
+
+		else
+
+		if (e.isAltDown() && e.getKeyChar() == 'c') {
+			System.out.println("clear");
+			explorer.clearView();
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+	}
+
+	public void keyTyped(KeyEvent e) {
+	}
+
+}
---
+++ src/net/trevize/wnxplorer/jwi/resultspanel/JWIWNResultsPanel.java
@@ -0,0 +1,216 @@
+package net.trevize.wnxplorer.jwi.resultspanel;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JEditorPane;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.StyleSheet;
+
+import net.trevize.wordnet.WNUtils;
+import edu.mit.jwi.IDictionary;
+import edu.mit.jwi.item.ISynset;
+
+/**
+ * 
+ * 
+ * @author Nicolas James <nicolas.james@gmail.com> [[http://njames.trevize.net]]
+ * JWIWNResultsPanel.java - Apr 6, 2010
+ */
+
+public class JWIWNResultsPanel {
+
+	public static final String STYLESHEET_FILEPATH = "./gfx/style.css";
+
+	private IDictionary dict;
+	private ArrayList<ISynset> results;
+
+	//swing components.
+	private JEditorPane view;
+	private HTMLEditorKit kit;
+
+	public static final int DEFAULT_NUMBER_OF_RESULTS_PER_PAGE = 5;
+	private int num_of_results_per_page;
+	private int current_page_number;
+
+	public JWIWNResultsPanel(IDictionary dict, ArrayList<ISynset> results) {
+		this.dict = dict;
+		this.results = results;
+		num_of_results_per_page = DEFAULT_NUMBER_OF_RESULTS_PER_PAGE;
+		current_page_number = 0;
+		init();
+	}
+
+	private void init() {
+		//create the jeditorpane.
+		view = new JEditorPane();
+		view.setEditable(false);
+		kit = new HTMLEditorKit();
+		view.setEditorKit(kit);
+
+		//loading the stylesheet.
+		StyleSheet ss = new StyleSheet();
+		StringBuffer sb = new StringBuffer();
+
+		try {
+			FileReader fr = new FileReader(STYLESHEET_FILEPATH);
+			BufferedReader br = new BufferedReader(fr);
+			sb = new StringBuffer();
+			String line;
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+			br.close();
+			fr.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		ss.addRule(sb.toString());
+		kit.getStyleSheet().addStyleSheet(ss);
+	}
+
+	public void retrieveResults(int idx_start, int idx_stop) {
+		StringBuffer sb = new StringBuffer();
+
+		sb.append("<ul>");
+
+		for (int result_id = idx_start; result_id < results.size()
+				&& result_id < idx_stop; ++result_id) {
+			sb.append("<li>");
+			sb.append(getResultHTMLFragment(result_id, results.get(result_id)));
+			sb.append("</li>");
+		}
+
+		sb.append("</ul>");
+		sb.append("</body></html>");
+
+		view.setText(sb.toString());
+	}
+
+	public void retrievePage(int page_number) {
+		retrieveResults(num_of_results_per_page * page_number,
+				num_of_results_per_page * (page_number + 1));
+		current_page_number = page_number;
+	}
+
+	private String getResultHTMLFragment(int result_id, ISynset synset) {
+		StringBuffer sb = new StringBuffer();
+
+		sb.append("[" + result_id + "] ");
+		String synset_id = synset.getID().toString();
+		sb.append("<a href=\"synset_id:" + synset_id + "\">");
+		sb.append(synset_id);
+		sb.append("</a>");
+		sb.append("<p>");
+		sb.append(getDocumentShortTextDescription(synset));
+		sb.append("</p>");
+
+		return sb.toString();
+	}
+
+	public int getPageCount() {
+		return results.size() / num_of_results_per_page + 1;
+	}
+
+	public boolean isTheFirstPage() {
+		return (current_page_number == 0);
+	}
+
+	public boolean isTheLastPage() {
+		return (current_page_number == (getPageCount() - 1));
+	}
+
+	public String getResultsStatus() {
+		StringBuffer sb = new StringBuffer();
+
+		int first_idx = current_page_number * num_of_results_per_page;
+		int last_idx = -1;
+
+		if (isTheLastPage()) {
+			last_idx = results.size() - 1;
+		} else {
+			last_idx = (current_page_number + 1) * num_of_results_per_page - 1;
+		}
+
+		sb.append(first_idx);
+		sb.append(" - ");
+		sb.append(last_idx);
+		sb.append(" | ");
+		sb.append(results.size() + " results");
+
+		return sb.toString();
+	}
+
+	public String getResultsStatusHTML() {
+		StringBuffer sb = new StringBuffer();
+
+		sb
+				.append("<html><body><div style=\"font-size:12px;text-align:center;\">");
+
+		int first_idx = current_page_number * num_of_results_per_page;
+		int last_idx = -1;
+
+		if (isTheLastPage()) {
+			last_idx = results.size() - 1;
+		} else {
+			last_idx = (current_page_number + 1) * num_of_results_per_page - 1;
+		}
+
+		sb.append(first_idx);
+		sb.append(" - ");
+		sb.append(last_idx);
+		sb.append(" | ");
+		sb.append(results.size() + " results");
+
+		sb.append("</div></body></html>");
+
+		return sb.toString();
+	}
+
+	public static String getDocumentShortTextDescription(ISynset synset) {
+		StringBuffer sb = new StringBuffer();
+
+		sb.append(synset.getID().toString());
+		sb.append("\n");
+
+		sb.append(WNUtils.getWords(synset));
+		sb.append("\n");
+		sb.append(synset.getGloss());
+
+		return sb.toString();
+	}
+
+	/***************************************************************************
+	 * getters and setters.
+	 **************************************************************************/
+
+	public int getNum_of_results_per_page() {
+		return num_of_results_per_page;
+	}
+
+	public void setNum_of_results_per_page(int numOfResultsPerPage) {
+		num_of_results_per_page = numOfResultsPerPage;
+	}
+
+	public int getCurrent_page_number() {
+		return current_page_number;
+	}
+
+	public void setCurrent_page_number(int currentPageNumber) {
+		current_page_number = currentPageNumber;
+	}
+
+	public JEditorPane getView() {
+		return view;
+	}
+
+	public void setView(JEditorPane view) {
+		this.view = view;
+	}
+
+}
