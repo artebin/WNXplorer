04814c0023b6bcf307a542d94676760fa2c41464
nicolasj <nicolasj@94909ec9-e09f-23db-a905-0964dd708685>
2010-11-02 09:24:35 +0000

    symmetric relations are now managed

--- trunk/src/net/trevize/wnxplorer/explorer/WNGraph.java
+++ trunk/src/net/trevize/wnxplorer/explorer/WNGraph.java
@@ -32,21 +32,22 @@ public class WNGraph {
 	private boolean show_all_vertices;
 	private HashMap<Pointer, Boolean> pointers_list;
 
-	//to index the "ever encountered" synsets.
-	//vertices are indexed by the synset ID.
-	private HashMap<String, SynsetVertex> vertex_idx_0;
+	/*
+	 * index of the vertices by synset ID?
+	 */
+	private HashMap<String, SynsetVertex> vertex_idx;
 
 	public WNGraph(IDictionary dict) {
 		this.dict = dict;
 		full_graph = new DirectedSparseMultigraph<SynsetVertex, PointerEdge>();
 		viewed_graph = new DirectedSparseMultigraph<SynsetVertex, PointerEdge>();
-		vertex_idx_0 = new HashMap<String, SynsetVertex>();
+		vertex_idx = new HashMap<String, SynsetVertex>();
 	}
 
 	public void clear() {
 		full_graph = new DirectedSparseMultigraph<SynsetVertex, PointerEdge>();
 		viewed_graph = new DirectedSparseMultigraph<SynsetVertex, PointerEdge>();
-		vertex_idx_0 = new HashMap<String, SynsetVertex>();
+		vertex_idx = new HashMap<String, SynsetVertex>();
 	}
 
 	public SynsetVertex addVertexForSynset(ISynset synset) {
@@ -58,66 +59,132 @@ public class WNGraph {
 		viewed_graph.addVertex(synset_vertex);
 
 		//index the vertex.
-		vertex_idx_0.put(synset_vertex.getSynset_id(), synset_vertex);
+		vertex_idx.put(synset_vertex.getSynset_id(), synset_vertex);
 
 		applyRenderingFilter();
 
 		return synset_vertex;
 	}
 
-	public boolean edgeExistsBetweenVerticesForRelationType(
+	public boolean edgeExistsBetweenVerticesForPointerType(
 			SynsetVertex vertex_src, SynsetVertex vertex_dest,
-			Pointer relation_type) {
+			Pointer pointer_type) {
 		Iterator<PointerEdge> edges_iter = full_graph.getOutEdges(vertex_src)
 				.iterator();
+
 		boolean found = false;
 		while (edges_iter.hasNext()) {
 			PointerEdge edge = edges_iter.next();
 			if (full_graph.getDest(edge) == vertex_dest) {
-				if (edge.getPointer_type().equals(relation_type)) {
+				if (edge.getPointer_type() == pointer_type) {
 					found = true;
 					break;
 				}
 			}
 		}
+
 		return found;
 	}
 
 	public void develop(SynsetVertex picked_vertex) {
-		//retrieving an ISynset from the vertex.
-		ISynset picked_synset = dict.getSynset(WNUtils
-				.getISynsetIDFromString(picked_vertex.getSynset_id()));
-
 		for (Pointer pointer : WNUtils.getPointers()) {
-			List<ISynsetID> pointered_synsets = picked_synset
+
+			List<ISynsetID> pointered_synsets = picked_vertex.getIsynset()
 					.getRelatedSynsets(pointer);
-			for (ISynsetID next_sid : pointered_synsets) {
-				ISynset sid = dict.getSynset(next_sid);
+
+			for (ISynsetID synset_id : pointered_synsets) {
+
+				ISynset pointered_synset = dict.getSynset(synset_id);
 
 				//is this synset ever present in the graph ?
-				SynsetVertex v = vertex_idx_0.get(sid.getID().toString());
+				SynsetVertex v = vertex_idx.get(pointered_synset.getID()
+						.toString());
+
 				if (v != null) {
-					if (edgeExistsBetweenVerticesForRelationType(picked_vertex,
+					if (edgeExistsBetweenVerticesForPointerType(picked_vertex,
 							v, pointer)) {
 						continue;
 					}
-					if (edgeExistsBetweenVerticesForRelationType(v,
-							picked_vertex, pointer)) {
+
+					/*
+					 * managing symmetric relations.
+					 */
+					if (pointer == Pointer.HYPERNYM
+							&& edgeExistsBetweenVerticesForPointerType(v,
+									picked_vertex, Pointer.HYPONYM)) {
+						continue;
+					}
+					
+					if (pointer == Pointer.HYPONYM
+							&& edgeExistsBetweenVerticesForPointerType(v,
+									picked_vertex, Pointer.HYPERNYM)) {
+						continue;
+					}
+
+					if (pointer == Pointer.HYPERNYM_INSTANCE
+							&& edgeExistsBetweenVerticesForPointerType(v,
+									picked_vertex, Pointer.HYPONYM_INSTANCE)) {
+						continue;
+					}
+					
+					if (pointer == Pointer.HYPONYM_INSTANCE
+							&& edgeExistsBetweenVerticesForPointerType(v,
+									picked_vertex, Pointer.HYPERNYM_INSTANCE)) {
+						continue;
+					}
+
+					if (pointer == Pointer.HOLONYM_PART
+							&& edgeExistsBetweenVerticesForPointerType(v,
+									picked_vertex, Pointer.MERONYM_PART)) {
+						continue;
+					}
+					
+					if (pointer == Pointer.MERONYM_PART
+							&& edgeExistsBetweenVerticesForPointerType(v,
+									picked_vertex, Pointer.HOLONYM_PART)) {
+						continue;
+					}
+
+					if (pointer == Pointer.HOLONYM_SUBSTANCE
+							&& edgeExistsBetweenVerticesForPointerType(v,
+									picked_vertex, Pointer.MERONYM_SUBSTANCE)) {
+						continue;
+					}
+					
+					if (pointer == Pointer.MERONYM_SUBSTANCE
+							&& edgeExistsBetweenVerticesForPointerType(v,
+									picked_vertex, Pointer.HOLONYM_SUBSTANCE)) {
+						continue;
+					}
+
+					if (pointer == Pointer.HOLONYM_MEMBER
+							&& edgeExistsBetweenVerticesForPointerType(v,
+									picked_vertex, Pointer.MERONYM_MEMBER)) {
+						continue;
+					}
+					
+					if (pointer == Pointer.MERONYM_MEMBER
+							&& edgeExistsBetweenVerticesForPointerType(v,
+									picked_vertex, Pointer.HOLONYM_MEMBER)) {
 						continue;
 					}
 				}
 
-				//add the vertex.
-				SynsetVertex v1 = addVertexForSynset(sid);
+				if (v == null) {
+					//add the vertex.
+					v = addVertexForSynset(pointered_synset);
+				}
 
 				//create the edge.
 				PointerEdge e1 = new PointerEdge(pointer);
 
 				//add the edge to the graph.
-				full_graph.addEdge(e1, picked_vertex, v1);
-				viewed_graph.addEdge(e1, picked_vertex, v1);
-			}
-		}
+				full_graph.addEdge(e1, picked_vertex, v);
+				viewed_graph.addEdge(e1, picked_vertex, v);
+
+			}//end for all pointered synsets.
+
+		}//end for all pointers types.
 
 		applyRenderingFilter();
 	}
