d2a34e4c8395a8e45c875fa5dd53f0c6a386f22c
nicolas.james <nicolas.james@gmail.com>
2011-04-21 11:59:39 +0000

    refactoring and documentation

--- src/net/trevize/wnxplorer/jung/PickingGraphMousePlugin.java
+++ src/net/trevize/wnxplorer/jung/PickingGraphMousePlugin.java
@@ -4,11 +4,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.geom.Point2D;
 
-import javax.swing.SwingUtilities;
-
 import net.trevize.wnxplorer.explorer.Explorer;
-import net.trevize.wnxplorer.jwi.WNUtils;
-import edu.mit.jwi.item.ISynset;
 import edu.uci.ics.jung.algorithms.layout.GraphElementAccessor;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 
@@ -48,9 +44,11 @@ public class PickingGraphMousePlugin
 			if (pickSupport != null) {
 				SynsetVertex v = pickSupport.getVertex(explorer
 						.getWngraph_panel().getLayout(), p.getX(), p.getY());
-				explorer.getWngraph_panel().developNode(v);
-			}
-		}
+				if (e.getButton() == MouseEvent.BUTTON1 && v != null) {
+					explorer.getWngraph_panel().developNode(v);
+				}
+			}//end pickSupport is not null.
+		}//end it is a double click.
 	}
 
 	@Override
@@ -87,42 +85,7 @@ public class PickingGraphMousePlugin
 					&& v != null
 					&& !v.getSynset_id().equals(
 							explorer.getSynset_info_panel().getSynset_id())) {
-
-				//retrieve an ISynset from the SynsetVertex.
-				ISynset synset = Explorer.wn_jwi_dictionary.getSynset(WNUtils
-						.getISynsetIDFromString(v.getSynset_id()));
-
-				//set the new content of the synset_info_panel.
-				explorer.getSynset_info_panel().updateContent(synset);
-
-				/*
-				 * set the synset_id in the synset_info_panel.
-				 * (this field is used for not displaying several times the same
-				 * synset_info_panel because of several clicks over the same node.
-				 */
-				explorer.getSynset_info_panel().setSynset_id(v.getSynset_id());
-
-				/*
-				 * put the scrollbars value of the scrollpane that contained the JeditorPane
-				 * to 0. 
-				 * Because of the JEditorPane, this has to be done in a separate thread.
-				 */
-				SwingUtilities.invokeLater(new Runnable() {
-					public void run() {
-						explorer.getSynset_info_panel().getScrollpane()
-								.getVerticalScrollBar().setValue(0);
-						explorer.getSynset_info_panel().getScrollpane()
-								.getHorizontalScrollBar().setValue(0);
-					}
-				});
-
-				/*
-				 * update the last_clicked_vertex field and repaint the graph
-				 */
-				explorer.getWngraph_panel().setLast_clicked_vertex(v);
-				
-				//refresh the views
-				explorer.refreshViews();
+				explorer.getWngraph_panel().selectNode(v);
 			}//end vertex not null and vertex is not the last_clicked_vertex.
 		}//end pickSupport is not null.
 	}
