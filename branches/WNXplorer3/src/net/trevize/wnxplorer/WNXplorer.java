4e3b7f2ea6186b64b23b16d247bf92f98640d726
nicolas.james@gmail.com <nicolas.james@gmail.com@94909ec9-e09f-23db-a905-0964dd708685>
2011-05-20 08:38:52 +0000

    Starting a new branch for properly implement the jwiknetvis interface (the access to the WordNet database through JWI has to be reserved to these interfaces. No usage of JWI somewhere else in the WNXplorer code).

---
+++ branches/WNXplorer3/src/net/trevize/wnxplorer/WNXplorer.java
@@ -0,0 +1,49 @@
+package net.trevize.wnxplorer;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+
+/**
+ * 
+ * 
+ * @author Nicolas James <nicolas.james@gmail.com> [[http://njames.trevize.net]]
+ * Main.java - Mar 24, 2010
+ */
+
+public class WNXplorer {
+
+	public static void main(String[] args) {
+		//setting the look and feel
+		if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+			try {
+				UIManager
+						.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (UnsupportedLookAndFeelException e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				UIManager
+						.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (UnsupportedLookAndFeelException e) {
+				e.printStackTrace();
+			}
+		}
+
+		Explorer explorer = new Explorer();
+	}
+
+}
