b591a365f419acbe0cf534f9d3a5f0bfdb2031ef
nicolas.james <nicolas.james@gmail.com>
2011-05-20 08:27:01 +0000

    merge of WNXplorer2 with the trunk

---
+++ src/net/trevize/wnxplorer/ResultsPanel.java
@@ -0,0 +1,203 @@
+package net.trevize.wnxplorer;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JEditorPane;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.StyleSheet;
+
+import net.trevize.wnxplorer.jwiknetvis.JWIUtils;
+
+import edu.mit.jwi.item.ISynset;
+
+/**
+ * 
+ * 
+ * @author Nicolas James <nicolas.james@gmail.com> [[http://njames.trevize.net]]
+ * ResultsPanel.java - Apr 6, 2010
+ */
+
+public class ResultsPanel {
+
+	private ArrayList<ISynset> results;
+
+	//swing components.
+	private JEditorPane view;
+	private HTMLEditorKit kit;
+
+	private int num_of_results_per_page = WNXplorerProperties
+			.getNum_of_results_per_page_into_search_panel();
+	private int current_page_number;
+
+	public ResultsPanel(ArrayList<ISynset> results) {
+		this.results = results;
+		current_page_number = 0;
+		init();
+	}
+
+	private void init() {
+		//create a new JEditorPane derived for setting ANTIALIASING ON
+		view = new JEditorPane() {
+			public void paintComponent(Graphics g) {
+				Graphics2D g2d = (Graphics2D) g;
+				g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+						RenderingHints.VALUE_ANTIALIAS_ON);
+				super.paintComponent(g);
+			}
+		};
+		view.setEditable(false);
+		kit = new HTMLEditorKit();
+		view.setEditorKit(kit);
+
+		//loading the stylesheet.
+		StyleSheet ss = new StyleSheet();
+		StringBuffer sb = new StringBuffer();
+
+		try {
+			FileReader fr = new FileReader(
+					WNXplorerProperties.getCss_path_main());
+			BufferedReader br = new BufferedReader(fr);
+			sb = new StringBuffer();
+			String line;
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+			br.close();
+			fr.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		ss.addRule(sb.toString());
+		kit.getStyleSheet().addStyleSheet(ss);
+	}
+
+	/**
+	 * 0 < idx_start < number of results aka results list size
+	 * 1 < idx_stop <= number_of_results aka results list size
+	 * 
+	 * the results at idx_stop is not taken in account, i.e. from idx_start to idx_stop-1
+	 */
+	public void retrieveResults(int idx_start, int idx_stop) {
+		StringBuffer sb = new StringBuffer();
+
+		sb.append("<html><body class=\"style1\">");
+
+		sb.append("<ul>");
+
+		for (int result_id = idx_start; result_id < results.size()
+				&& result_id < idx_stop; ++result_id) {
+			sb.append("<li>");
+			sb.append("[" + (result_id + 1) + "] ");
+			String synset_id = results.get(result_id).getID().toString();
+			sb.append("<a href=\"synset_id:" + synset_id + "\">");
+			sb.append(synset_id);
+			sb.append("</a>");
+			sb.append("<p>");
+			sb.append("<b>" + JWIUtils.getWords(results.get(result_id)) + "</b>");
+			sb.append("\n");
+			sb.append(results.get(result_id).getGloss());
+			sb.append("</p>");
+			sb.append("</li>");
+		}
+
+		sb.append("</ul>");
+		sb.append("</body></html>");
+
+		view.setText(sb.toString());
+	}
+
+	public void retrievePage(int page_number) {
+		retrieveResults(num_of_results_per_page * page_number,
+				num_of_results_per_page * (page_number + 1));
+		current_page_number = page_number;
+	}
+
+	public int getPageCount() {
+		return results.size() / num_of_results_per_page
+				+ ((results.size() % num_of_results_per_page == 0) ? 0 : 1);
+	}
+
+	public boolean isTheFirstPage() {
+		return (current_page_number == 0);
+	}
+
+	public boolean isTheLastPage() {
+		return (current_page_number == (getPageCount() - 1));
+	}
+
+	public String getResultsStatus() {
+		StringBuffer sb = new StringBuffer();
+
+		if (results.size() != 0) {
+			int first_idx = current_page_number * num_of_results_per_page;
+			int last_idx = -1;
+
+			if (isTheLastPage()) {
+				last_idx = results.size() - 1;
+			} else {
+				last_idx = (current_page_number + 1) * num_of_results_per_page
+						- 1;
+			}
+
+			sb.append(first_idx + 1);
+			sb.append(" - ");
+			sb.append(last_idx + 1);
+			sb.append(" | ");
+		}
+
+		sb.append(results.size() + " results");
+
+		return sb.toString();
+	}
+
+	public String getResultsStatusHTML() {
+		StringBuffer sb = new StringBuffer();
+
+		sb.append("<html><body><div style=\"font-size:12px;text-align:center;\">");
+
+		if (results.size() != 0) {
+			int first_idx = current_page_number * num_of_results_per_page;
+			int last_idx = -1;
+
+			if (isTheLastPage()) {
+				last_idx = results.size() - 1;
+			} else {
+				last_idx = (current_page_number + 1) * num_of_results_per_page
+						- 1;
+			}
+
+			sb.append(first_idx + 1);
+			sb.append(" - ");
+			sb.append(last_idx + 1);
+			sb.append(" | ");
+		}
+
+		sb.append(results.size() + " results");
+
+		sb.append("</div></body></html>");
+
+		return sb.toString();
+	}
+
+	/***************************************************************************
+	 * getters and setters.
+	 **************************************************************************/
+
+	public JEditorPane getView() {
+		return view;
+	}
+
+	public int getCurrent_page_number() {
+		return current_page_number;
+	}
+
+}
---
+++ src/net/trevize/wnxplorer/dialogs/AboutDialog.java
@@ -0,0 +1,178 @@
+package net.trevize.wnxplorer.dialogs;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+
+import net.trevize.wnxplorer.WNXplorerProperties;
+
+import org.xhtmlrenderer.simple.FSScrollPane;
+import org.xhtmlrenderer.simple.XHTMLPanel;
+import org.xhtmlrenderer.swing.BasicPanel;
+import org.xhtmlrenderer.swing.FSMouseListener;
+import org.xhtmlrenderer.swing.LinkListener;
+import org.xhtmlrenderer.swing.SelectionHighlighter;
+
+public class AboutDialog extends JDialog implements WindowListener,
+		HyperlinkListener {
+
+	private JPanel main_panel;
+	private XHTMLPanel xhtml_panel;
+	private FSScrollPane scrollpane;
+	private JButton close_button;
+
+	public AboutDialog(JComponent parent) {
+		setModalityType(ModalityType.APPLICATION_MODAL);
+		setTitle("About");
+		init();
+		setIconImage(new ImageIcon(WNXplorerProperties.getIcon_path_wnxplorer())
+				.getImage());
+		setSize(442, 442);
+		setResizable(false);
+		setLocationRelativeTo(parent);
+	}
+
+	private void init() {
+		//setting swing components.
+		main_panel = new JPanel();
+		setLayout(new BorderLayout());
+		add(main_panel, BorderLayout.CENTER);
+		main_panel.setLayout(new BorderLayout());
+		main_panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+		scrollpane = new FSScrollPane();
+		main_panel.add(scrollpane, BorderLayout.CENTER);
+
+		xhtml_panel = new XHTMLPanel();
+		xhtml_panel.setDocument(new File(WNXplorerProperties
+				.getHtml_path_about()).toURI().toString());
+		xhtml_panel.getSharedContext().getTextRenderer()
+				.setSmoothingThreshold(1.f);
+
+		//remove the default LinkListener
+		List<FSMouseListener> mouse_tracking_listeners = xhtml_panel
+				.getMouseTrackingListeners();
+		for (FSMouseListener listener : mouse_tracking_listeners) {
+			if (listener instanceof LinkListener) {
+				xhtml_panel.removeMouseTrackingListener(listener);
+			}
+		}
+
+		//install our LinkListener
+		xhtml_panel.addMouseTrackingListener(new LinkListener() {
+			@Override
+			public void linkClicked(BasicPanel panel, String uri) {
+				try {
+					java.awt.Desktop.getDesktop().browse(new URI(uri));
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (URISyntaxException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+
+		SelectionHighlighter sh = new SelectionHighlighter();
+		sh.install(xhtml_panel);
+
+		scrollpane.setViewportView(xhtml_panel);
+		scrollpane
+				.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollpane
+				.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+		//create a panel for the close button at the bottom of the HelpDialog
+		//the Close button is centered using two Box.createGlue()
+		JPanel panel0 = new JPanel();
+		panel0.setBorder(new EmptyBorder(10, 0, 0, 0));
+		panel0.setLayout(new BoxLayout(panel0, BoxLayout.X_AXIS));
+		panel0.add(Box.createGlue());
+		close_button = new JButton("Close");
+		close_button.setMnemonic('C');
+		close_button.setIcon(new ImageIcon(WNXplorerProperties
+				.getIcon_path_close()));
+		close_button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
+		panel0.add(close_button);
+		panel0.add(Box.createGlue());
+		main_panel.add(panel0, BorderLayout.SOUTH);
+	}
+
+	/***************************************************************************
+	* implementation of WindowListener.
+	**************************************************************************/
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		setVisible(false);
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+	}
+
+	/***************************************************************************
+	* implementation of HyperlinkListener.
+	**************************************************************************/
+
+	@Override
+	public void hyperlinkUpdate(HyperlinkEvent e) {
+		if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+			System.out.println("link clicked");
+			System.out.println(e.getDescription());
+
+			try {
+				java.awt.Desktop.getDesktop().browse(
+						new URI(e.getDescription()));
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			} catch (URISyntaxException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+}
